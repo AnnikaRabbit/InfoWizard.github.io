44d7b2cd6bb38d561322202e4207a20e71604a2f
VTM-FAME <fame@Varjac.local>
2017-03-26 20:40:37 -0500

    edu porject
    edu prokect

---
+++ eduframeworks/js/design.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+
+  $("#lfu-button").click(function() {
+      window.location = "lfu.html";
+  });
+
+  $("#skif-button").click(function() {
+      window.location = "skif.html";
+  });
+
+  $("#sdf-button").click(function() {
+      window.location = "sdf.html";
+  });
+
+  $("#con-button").click(function() {
+      window.location = "construct.html";
+  });
+
+  $("#ai-button").click(function() {
+      window.location = "ai.html";
+  });
+
+  $("#ci-button").click(function() {
+      window.location = "ci.html";
+  });
+
+  $("#menuButton").click(function() {
+      window.location = "index.html";
+  });
+
+});
