27dd47e157c8f38269a20508e1bd61fe2a7b7291
VTM-FAME <fame@varjac.attlocal.net>
2017-04-12 18:38:29 -0500

    version update

--- eduframeworks/js/design.js
+++ eduframeworks/js/design.js
@@ -1,33 +1,34 @@
 $(document).ready(function(){
 
+  // 
+  // $("#lfu-button").click(function() {
+  //     window.location = "lfu.html";
+  // });
+  //
+  // $("#skif-button").click(function() {
+  //     window.location = "skif.html";
+  // });
+  //
+  // $("#sdf-button").click(function() {
+  //     window.location = "sdf.html";
+  // });
+  //
+  // $("#con-button").click(function() {
+  //     window.location = "construct.html";
+  // });
+  //
+  // $("#ai-button").click(function() {
+  //     window.location = "ai.html";
+  // });
+  //
+  // $("#ci-button").click(function() {
+  //     window.location = "ci.html";
+  // });
 
-  $("#lfu-button").click(function() {
-      window.location = "lfu.html";
-  });
+  // $("#menuButton").click(function() {
+  //     window.location = "index.html";
+  // });
 
-  $("#skif-button").click(function() {
-      window.location = "skif.html";
-  });
-
-  $("#sdf-button").click(function() {
-      window.location = "sdf.html";
-  });
-
-  $("#con-button").click(function() {
-      window.location = "construct.html";
-  });
-
-  $("#ai-button").click(function() {
-      window.location = "ai.html";
-  });
-
-  $("#ci-button").click(function() {
-      window.location = "ci.html";
-  });
-
-  $("#menuButton").click(function() {
-      window.location = "index.html";
-  });
 
 
 });
