1f4cc95c70b0a6b032b964d59d99b86e2ce4e0f5
VTM-FAME <fame@varjac.attlocal.net>
2017-05-06 16:20:56 -0500

    projects js update

--- portfolio/js/projects.js
+++ portfolio/js/projects.js
@@ -31,20 +31,20 @@ var projectItems = [];
 projectItems.push(new Article("Hearthstone \“Geek out\” fansite",
     "images/geekout.png",
     "https://nicholaspersa.github.io/geekout/",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "As a avid player of Hearthstone, I took on a design challenge related to the game. The site is a brief introduction to the game and acted as practice for vector-based assets. I made all of the artwork using Sketch (except the wood pattern).",
     true));
 
-projectItems.push(new Article("Hearthstone \“Geek out\” fansite",
-    "images/geekout.png",
-    "https://nicholaspersa.github.io/geekout/",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+projectItems.push(new Article("Designing Curriculum Primer",
+    "images/eduFrameworks.png",
+    "https://nicholaspersa.github.io/eduframeworks/",
+    "A small web project focused on displaying content about designing curriculum. The project functions as a primer and reference document for those who design educational experiences.",
     true));
 
-projectItems.push(new Article("Pokemon Go Fansite",
-    "images/gogreet.png",
-    "http://nicholaspersa.github.io/gogreet",
-    "",
-    false));
+projectItems.push(new Article("Photography & Poetry: \"The Beacon\"",
+    "images/beacon.png",
+    "https://nicholaspersa.github.io/archive/",
+    "A photography project that documents the location of one building from a variety of perspectives in Minnesota. I have lived near this building a majority of my life, and I can't seem to escape its view. It felt proper to give it one more perspective to be seen from.",
+    true));
 
 projectItems.push(new Article("File Visualizer",
     "images/tldr.png",
@@ -52,8 +52,40 @@ projectItems.push(new Article("File Visualizer",
     "",
     false));
 
-projectItems.push(new Article("Photography & Poetry",
-    "images/beacon.png",
-    "http://nicholaspersa.github.io/archive",
+projectItems.push(new Article("Bucket Run",
+    "images/bucketRun.png",
+    "http://nicholaspersa.github.io/bucketrun",
+    "",
+    false));
+projectItems.push(new Article("Experimental Web II",
+    "images/clickVenture.png",
+    "http://nicholaspersa.github.io/singleServe",
+    "",
+    false));
+projectItems.push(new Article("Experimental Web I",
+    "images/classExp.png",
+    "http://nicholaspersa.github.io/classExp",
+    "",
+    false));
+
+projectItems.push(new Article("Pokemon Go Fansite",
+    "images/gogreet.png",
+    "http://nicholaspersa.github.io/gogreet",
+    "",
+    false));
+
+projectItems.push(new Article("Lucca Website Rework",
+    "images/lucca.png",
+    "http://nicholaspersa.github.io/lucca",
+    "",
+    false));
+projectItems.push(new Article("Theory and Theorist",
+    "images/theoryAnd.png",
+    "http://nicholaspersa.github.io/theory",
+    "",
+    false));
+projectItems.push(new Article("Theory Stories",
+    "images/story.png",
+    "http://nicholaspersa.github.io/story",
     "",
     false));
